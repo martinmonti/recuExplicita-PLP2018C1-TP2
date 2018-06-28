d99ab827f553edf86bd587cdf494f63d896d180b
mam <martin.monti@sisa.com.ar>
2018-06-28 12:07:21 -0300

    Subo Tests para Ejs de 1 a 7

--- esqueleto.pl
+++ esqueleto.pl
@@ -67,11 +67,12 @@ simbolos(C) :- findall(X,symbol(X),C).
 
 % Ejercicio 4: match_inst(+Cadena, +RegEx)
 
-match_inst([],RE) :- RE = empty,!.
+match_inst([],empty) :- !.
 match_inst([CH],RE) :- symbol(RE), CH = RE, !.
 match_inst(C,or(RE1,RE2)) :- match_inst(C,RE1),!.
 match_inst(C,or(RE1,RE2)) :- match_inst(C,RE2),!.
 match_inst(C,concat(RE1,RE2)):- prefix(PREF,C), append(PREF,SUF,C), match_inst(PREF,RE1),match_inst(SUF,RE2),!.
+match_inst([],star(RE)):-!.
 match_inst(C,star(RE)) :- prefix(PREF,C), append(PREF,RESTO,C), match_inst(PREF,RE), match_inst(RESTO,empty),!.
 match_inst(C,star(RE)) :- prefix(PREF,C), append(PREF,RESTO,C), match_inst(PREF,RE), match_inst(RESTO,star(RE)),!.
 
@@ -87,7 +88,25 @@ testEj4() :- match_inst([], empty)
 				,not(match_inst([a,a,b],concat(a,b)))
 				,not(match_inst([a,c],concat(a,b)))
 				,match_inst([a, a, b], concat(star(a), b))
-				,not(match_inst([b, a], concat(star(a), b))).
+				,not(match_inst([b, a], concat(star(a), b)))
+				,match_inst([],star(a))
+				,match_inst([a],star(a))
+				,match_inst([a,a],star(a))
+				,match_inst([a,a,a],star(a))
+				,match_inst([a,a,a,a],star(a))
+				,not(match_inst([a,a,a,b],star(a)))
+				,not(match_inst([b,a,a,a],star(a)))
+				,not(match_inst([a,a,b,a],star(a)))
+				,match_inst([],star(concat(a,b)))
+				,match_inst([a,b],star(concat(a,b)))
+				,match_inst([a,b,a,b],star(concat(a,b)))
+				,not(match_inst([a,b,c],star(concat(a,b))))
+				,not(match_inst([a,b,a],star(concat(a,b))))
+				,not(match_inst([a,b,b,a],star(concat(a,b))))
+				,not(match_inst([a,b,a,b,a],star(concat(a,b))))
+				,not(match_inst([a,c,b],star(concat(a,b))))
+				,not(match_inst([b,a],star(concat(a,b))))
+				,not(match_inst([a,a,b,b],star(concat(a,b)))).
 
 % Ejercicio 5: match(?Cadena, +RegEx)
 
@@ -98,10 +117,42 @@ match(C,REGEX) :- var(C),cadenasCandidatasParaRegEx(C,REGEX),match_inst(C,REGEX)
 cadenasCandidatasParaRegEx(C,REGEX) :- tieneEstrella(REGEX),cadena(C).
 cadenasCandidatasParaRegEx(C,REGEX) :- not(tieneEstrella(REGEX)),longitudMaxima(REGEX,MAXL),cadenas(0,MAXL,[],C).
 
+testEj5() :- match([], empty)
+				,not(match([a], empty))
+				,match([a],a)
+				,not(match([b],a))
+				,not(match([a], b))
+				,match([b], b)
+				,not(match([c], or(a, b)))
+				,match([b], or(a, b))
+				,match([a,b],concat(a,b))
+				,not(match([a,a,b],concat(a,b)))
+				,not(match([a,c],concat(a,b)))
+				,match([a, a, b], concat(star(a), b))
+				,not(match([b, a], concat(star(a), b))).
+
 % Ejercicio 6: diferencia(?Cadena, +RegEx, +RegEx)
 
 diferencia(C,R1,R2) :- ground(C),match_inst(C,R1),not(match_inst(C,R2)),!.
 diferencia(C,R1,R2) :- var(C), cadenasCandidatasParaRegEx(C,R1),match_inst(C,R1),not(match_inst(C,R2)).
+
+testEj6() :- diferencia([a],star(a),empty)
+           , diferencia([a,a],star(a),empty)
+			  , not(diferencia([b],star(a),empty))
+			  , not(diferencia([],star(a),empty))
+			  , diferencia([a],or(a,b),b)
+			  , not(diferencia([b],or(a,b),b))
+			  , diferencia([a],star(or(a,b)),star(b))
+			  , diferencia([a,a],star(or(a,b)),star(b))
+			  , diferencia([a,b],star(or(a,b)),star(b))
+			  , diferencia([b,a],star(or(a,b)),star(b))
+			  , diferencia([a,a,a],star(or(a,b)),star(b))
+			  , diferencia([a,a,b],star(or(a,b)),star(b))
+			  , not(diferencia([b],star(or(a,b)),star(b)))
+			  , not(diferencia([b,b],star(or(a,b)),star(b)))
+			  , not(diferencia([c],star(or(a,b)),star(b)))
+			  , not(diferencia([c,a],star(or(a,b)),star(b)))
+			  , not(diferencia([c,b],star(or(a,b)),star(b))) .
 
 % Ejercicio 7: prefijoMaximo(?Prefijo, +Cadena, +RegEx)
 
@@ -120,6 +171,17 @@ encontrarListaMayor([L|LS],LAUX,L_MAX):- length(L,NL),length(LAUX,NLAUX),NL>NLAU
 
 esPrefijoYMatchea(PREF,CADENA,REGEX):- prefix(PREF,CADENA), match_inst(PREF,REGEX).
 
+testEj7() :-  prefijoMaximo([a,a,a],[a,a,a,b],star(a))
+            , not(prefijoMaximo([],[a,a,a,b],star(a)))
+            , not(prefijoMaximo([a],[a,a,a,b],star(a)))
+            , not(prefijoMaximo([a,a],[a,a,a,b],star(a)))
+            , not(prefijoMaximo([a,a,a,b],[a,a,a,b],star(a)))
+            , prefijoMaximo([],[a,a,a,b,b,a],empty)
+            , not(prefijoMaximo([a],[a,a,a,b,b,a],empty))
+            , prefijoMaximo([a,a,a,b,b],[a,a,a,b,b,a],concat(star(a),star(b)))
+            , not(prefijoMaximo([a,a,a],[a,a,a,b,b,a],concat(star(a),star(b))))
+            , not(prefijoMaximo([a,a,a,b,b,a],[a,a,a,b,b,a],concat(star(a),star(b)))).
+
 
 % Ejercicio 8: reemplazar(+X, +R, +E, Res)
 
@@ -135,3 +197,7 @@ reemplazar(CADENA,REGEX,REEMPLAZO,RESULTADO):-
 ,reemplazar(CADENA_CON_REEMPLAZO_HECHO,REGEX,REEMPLAZO,RESULTADO),!.
 
 substringQueMatchea(Cadena,Regex,SubStringMatch):-CS=[C1|R1],R1=[SubStringMatch|R2],R2=[C3],append(CS,Cadena),match_inst(SubStringMatch,Regex).
+
+testEj8() :- reemplazar([a,b],a,[c],[c,b])
+           , reemplazar([a,a,a,b,b],concat(star(a),b),[1],[1,1])
+           .
